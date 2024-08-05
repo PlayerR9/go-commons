cdd1d278d23ff000891a25bc7e625c731fa247b3
PlayerR9 <riateapad@gmail.com>
2024-08-05 14:18:42 +0200

    added utility functions

--- runes/runes_test.go
+++ runes/runes_test.go
@@ -23,3 +23,29 @@ func TestToInt(t *testing.T) {
 		t.Errorf("expected 'a' but got %c", char)
 	}
 }
+
+func TestFindContentIndexes(t *testing.T) {
+	const (
+		OpToken rune = '('
+		ClToken rune = ')'
+	)
+
+	var (
+		ContentTokens []rune = []rune{
+			'(', '(', 'a', '+', 'b', ')', '*', 'c', ')', '+', 'd',
+		}
+	)
+
+	indices, err := FindContentIndexes(OpToken, ClToken, ContentTokens)
+	if err != nil {
+		t.Errorf("expected no error, got %s instead", err.Error())
+	}
+
+	if indices[0] != 1 {
+		t.Errorf("expected 1, got %d instead", indices[0])
+	}
+
+	if indices[1] != 9 {
+		t.Errorf("expected 9, got %d instead", indices[1])
+	}
+}
