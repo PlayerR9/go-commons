3606e41d5c35491ed999ba0db69bfae6387e84d4
PlayerR9 <riateapad@gmail.com>
2024-09-22 14:27:14 +0200

    update

--- strings/conv.go
+++ strings/conv.go
@@ -5,6 +5,26 @@ import (
 	"strconv"
 )
 
+// Quote returns a string representation of the given element, enclosed in double
+// quotes.
+//
+// Parameters:
+//   - elem: The element to get the string representation of.
+//
+// Returns:
+//   - string: The string representation of the element. If the element is nil, the
+//     function returns an empty string enclosed in double quotes.
+func Quote(elem any) string {
+	if elem == nil {
+		return "\"\""
+	}
+
+	str := fmt.Sprintf("%v", elem)
+	str = strconv.Quote(str)
+
+	return str
+}
+
 // QuoteStrings is a function that quotes a slice of strings in-place.
 //
 // Parameters:
